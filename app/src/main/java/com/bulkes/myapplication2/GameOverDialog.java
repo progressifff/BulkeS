a663621e2786256469cc012d549a6953d03f46ee
vlad230596 <vlad230596@mail.ru>
2016-04-30 20:23:07 +0300

    Main form v. 1.1

--- app/src/main/java/com/bulkes/myapplication2/GameOverDialog.java
+++ app/src/main/java/com/bulkes/myapplication2/GameOverDialog.java
@@ -7,13 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 
 /**
  * Created by progr on 31.03.2016.
  */
 public class GameOverDialog extends Dialog {
-    private Button closeGameBtn;
+    private ImageButton closeGameBtn;
     private Button repeatGameBtn;
     AppCompatActivity activity;
 
@@ -30,16 +31,16 @@ public class GameOverDialog extends Dialog {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_game_over_layout);
         getWindow().getAttributes().windowAnimations = R.style.GameDialogAnimation;
-        closeGameBtn = (Button) findViewById(R.id.close_btn);
-        closeGameBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((GoGaming)activity).dialogEndGame();
-                //activity.finish();
-                cancel();
+        closeGameBtn = (ImageButton) findViewById(R.id.close_btn);
+//        closeGameBtn.setOnClickListener(new View.OnClickListener() {
+  //          @Override
+    //        public void onClick(View v) {
+      //          ((GoGaming)activity).dialogEndGame();
+          //      //activity.finish();
+        //        cancel();
 
-            }
-        });
+          //  }
+        //});
         repeatGameBtn = (Button) findViewById(R.id.repeate_btn);
         repeatGameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/bulkes/myapplication2/TestMySQL.java
+++ app/src/main/java/com/bulkes/myapplication2/TestMySQL.java
@@ -2,6 +2,7 @@ package com.bulkes.myapplication2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.ParseException;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -10,10 +11,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
+
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,6 +25,7 @@ import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 
 /**
@@ -31,7 +34,7 @@ import java.net.URL;
 public class TestMySQL extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
     private SwipeRefreshLayout swipeRefreshLayout;
     private Activity activity;
-  //  private URL url;
+    //  private URL url;
     private final String urlPost = "http://bulkes.orgfree.com/php/update.php";
     public static final String USER_NAME = "nickname";
     public static final String GAME_SCORE = "userscore";
@@ -107,147 +110,59 @@ public class TestMySQL extends AppCompatActivity implements SwipeRefreshLayout.O
             return null;
         }
         protected void onPostExecute(Void result) {
-            // dismiss progress dialog and update ui
+
         }
     }
 
     class ServerDataTask extends AsyncTask<Void,Void,Void>
     {
         private String result;
-        private OutputStreamWriter streamWriter;
+        private String response;
         private BufferedReader buferedReader;
-        private JSONArray jArray;
-
         String jName;
         int jGamescore;
         String jGametime;
-
         protected Void doInBackground(Void... params) {
             try {
-                int id = 10000;
-                String query = "id=" + id +"&name=" + name + "&gamescore=" + gamescore + "&gametime=" + gametime;
-
                 URL url = new URL("http://bulkes.orgfree.com/php/get.php");
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setReadTimeout(10000);
                 connection.setDoInput(true);
                 connection.setDoOutput(true);
-                streamWriter = new OutputStreamWriter(connection.getOutputStream());
-                streamWriter.write(query);
-                streamWriter.flush();
-                buferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                Gson gson = new Gson();
-             //   JsonArray jsonArray = new JsonParser().parse(buferedReader).getAsJsonArray();
-
-               // JsonParser jsonElement = new JsonParser();
-               // JsonObject result = jsonElement.parse(buferedReader).getAsJsonObject();
-
+                buferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"), 8);
                 StringBuilder sb = new StringBuilder();
-                // sb.append(buferedReader.readLine() + "\n");
-                String line="0";
-
-                while ((line = buferedReader.readLine()) != null) {
-                    //Log.v("EEE",line);
-                    sb.append(line + "\n");
+                sb.append(buferedReader.readLine() + "\n");
+                //  while ((line = buferedReader.readLine()) != null) {
+                //      //Log.v("EEE",line);b
+                //       sb.append(line + "\n");
+                //   }
+                response = sb.toString();
+                result = response.substring(response.indexOf('['));
+                JSONArray array = new JSONArray(result);
+                JSONObject jsonData;
+                for (int i = 0; i < array.length(); i++) {
+                    jsonData = array.getJSONObject(i);
+                    jName = jsonData.getString("name");
+                    jGamescore = jsonData.getInt("gamescore");
+                    jGametime = jsonData.getString("gamescore");
+                    Log.v("JSONArray", jName + " " + jGamescore + " " + jGametime);
                 }
-                JsonReader reader = new JsonReader(new StringReader(sb.toString()));
-                reader.setLenient(true);
-                reader.beginArray();
-                while (reader.hasNext()) {
-                    Log.v("GGGGGG",reader.getPath());
-                }
-                reader.endArray();
-                //Log.v("GGGGGG",reader.toString());
-               // reader.
-               // jName = gson.fromJson(reader, String.class);
-               // jGamescore = gson.fromJson(reader,Integer.class);
-              //  jGametime = gson.fromJson(reader,String.class);
-
-/*
-                Set<Map.Entry<String, JsonElement>> entrySet = result.entrySet();
-                for(Map.Entry<String, JsonElement> entry : entrySet) {
-                    Log.v("RRRRRR",entry.getKey());
-                   /* jName = gson.fromJson(result(entry.getKey()), String.class);
-                    jGamescore = gson.fromJson(jsonElement,Integer.class);
-                    jGametime = gson.fromJson(jsonElement,String.class);
-                    Log.v("JSONArray",jName+" "+jGamescore+" "+jGametime);
-                    */
-             //   }
-/*
-                Set<Map.Entry<String, JsonElement>> entrySet = result.entrySet();
-                for(Map.Entry<String, JsonElement> entry : entrySet) {
-                    User newUser = gson.fromJson(p.getAsJsonObject(entry.getKey()), User.class);
-                    newUser.username = entry.getKey();
-                    //code...
-                }
-
-                for (int i = 0; i < jsonArray.size(); i++) {
-                    JsonElement jsonElement = jsonArray.get(i);
-                    JsonObject jsonObject = jsonElement.getAsJsonObject();
-
-                    Set<Map.Entry<String, JsonElement>> entrySet = jsonObject.entrySet();
-                    for(Map.Entry<String, JsonElement> entry : entrySet) {
-                        User newUser = gson.fromJson(p.getAsJsonObject(entry.getKey()), User.class);
-                        newUser.username = entry.getKey();
-                        //code...
-                    }
-
-
-                }
-                StringBuilder sb = new StringBuilder();
-               // sb.append(buferedReader.readLine() + "\n");
-                String line="0";
-                while ((line = buferedReader.readLine()) != null) {
-                    Log.v("EEE",line);
-                    sb.append(line + "\n");
-                }
-                */
-                streamWriter.close();
-                //result = sb.toString();
-               // Log.v("RESPONSEEEEEEE",sb.toString());
             } catch (MalformedURLException e) {
                 Log.v("MalformedURLException", e.toString());
             } catch (IOException e) {
                 Log.v("IOException", e.toString());
             } catch (Exception e) {
                 Log.v("Some", e.toString());
-            }
-            finally {
+            } finally {
                 try {
-                   // streamWriter.close();
                     buferedReader.close();
+                } catch (Exception ex) {
                 }
-                catch(Exception ex) {}
             }
-
-
-
-/*
-            String jName;
-            int jGamescore;
-            String jGametime;
-            try{
-            //    JSONArray jsonArray = new JsonParser().parse(br).getAsJsonArray();
-                jArray = new JSONArray(result);
-                JSONObject jsonData;
-                for(int i=0;i<jArray.length();i++){
-                    jsonData = jArray.getJSONObject(i);
-                    jName = jsonData.getString("name");
-                    jGamescore = jsonData.getInt("gamescore");
-                    jGametime = jsonData.getString("gamescore");
-                    Log.v("JSONArray",jName+" "+jGamescore+" "+jGametime);
-                }
-
-            }catch(JSONException e1){
-                Log.v("JSONException", e1.toString());
-            }catch (ParseException e1){
-                Log.v("ParseException", e1.toString());
-            }
-*/
             return null;
         }
         protected void onPostExecute(Void result) {
-            // dismiss progress dialog and update ui
+
         }
     }
 
@@ -257,13 +172,12 @@ public class TestMySQL extends AppCompatActivity implements SwipeRefreshLayout.O
         Toast.makeText(activity, "Refresh", Toast.LENGTH_SHORT).show();
         swipeRefreshLayout.setRefreshing(true);
 
-       /* swipeRefreshLayout.postDelayed(new Runnable() {
+        swipeRefreshLayout.postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                }
-
+            }
         }, 3000);
-*/
+
     }
 }
